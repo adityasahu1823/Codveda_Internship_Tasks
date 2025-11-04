eb4b62924d11886bc9315f789eb99fa1b7fa8a4a
Aditya Sahu <aditya7869sahu@gmail.com>
2025-11-04 21:18:15 +0530

    Implement basic calculator functionality

---
+++ Level1_Task1_BasicCalculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Level1_Task1_BasicCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Basic Calculator ===");
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose operation: +  -  *  /");
+        char operator = sc.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero is not allowed!");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+        sc.close();
+    }
+}
